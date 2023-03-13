f9ae0df0c2523c845843c25cc94e7c355607dcfb
Ebube Glory Ogbonda <ogbondaglory@gmail.com>
2023-03-13 17:47:17 +0000

    test axis
    axis labels

--- ICE-4711/js/appD3v3.js
+++ ICE-4711/js/appD3v3.js
@@ -41,3 +41,97 @@ myBars.attr({
     return color;
   }
 });
+
+// Add 3D axis to the chart
+var axis = d3.select("#axis")
+  .attr("position", "0 0 0")
+  .attr("rotation", "-90 0 0");
+
+axis.append("a-cylinder")
+  .attr("position", "0 0 " + chartDepth / 2)
+  .attr("height", chartWidth)
+  .attr("radius", 0.02)
+  .attr("rotation", "90 0 0")
+  .attr("color", "gray");
+
+axis.append("a-cylinder")
+  .attr("position", chartWidth / 2 + " 0 0")
+  .attr("height", chartDepth)
+  .attr("radius", 0.02)
+  .attr("rotation", "0 0 0")
+  .attr("color", "gray");
+
+// Add X-axis tick marks and labels
+for (var i = 0; i < gridMax; i++) {
+  var x = i * 1.1 - chartWidth / 2;
+  axis.append("a-cylinder")
+  .attr("position", x + " 0 " + chartDepth / 2)
+  .attr("height", 0.05)
+  .attr("radius", 0.02)
+  .attr("rotation", "90 0 0")
+  .attr("color", "gray");
+  axis.append("a-text")
+  .attr("value", i + 1)
+  .attr("position", x + " -0.1 " + chartDepth / 2)
+  .attr("rotation", "0 0 90")
+  .attr("align", "center")
+  .attr("width", "1")
+  .attr("color", "black");
+  }
+  
+  // Add Z-axis tick marks and labels
+  for (var i = 0; i < Math.ceil(dataset.length / gridMax); i++) {
+  var z = i * 1.1 - chartDepth / 2;
+  axis.append("a-cylinder")
+  .attr("position", "0 0 " + z)
+  .attr("height", 0.05)
+  .attr("radius", 0.02)
+  .attr("color", "gray");
+  axis.append("a-text")
+  .attr("value", i + 1)
+  .attr("position", "0 -0.1 " + z)
+  .attr("align", "center")
+  .attr("width", "1")
+  .attr("color", "black");
+  }
+  
+  // Add Y-axis tick marks and labels
+  for (var i = 0; i < 6; i++) {
+  var y = i * 5 / 4;
+  axis.append("a-cylinder")
+  .attr("position", "0 " + y + " 0")
+  .attr("height", 0.05)
+  .attr("radius", 0.02)
+  .attr("color", "gray");
+  axis.append("a-text")
+  .attr("value", i * 5)
+  .attr("position", "-0.1 " + y + " 0")
+  .attr("align", "right")
+  .attr("width", "1")
+  .attr("color", "black");
+  }
+  
+  // Add axis labels
+  axis.append("a-text")
+  .attr("value", "X Axis")
+  .attr("position", chartWidth / 2 + " -0.1 " + chartDepth / 2)
+  .attr("rotation", "0 0 -90")
+  .attr("align", "center")
+  .attr("width", "2")
+  .attr("color", "black");
+  
+  axis.append("a-text")
+  .attr("value", "Y Axis")
+  .attr("position", "0 " + (chartDepth / 2 + 0.2) + " 0")
+  .attr("rotation", "-90 0 0")
+  .attr("align", "center")
+  .attr("width", "2")
+  .attr("color", "black");
+  
+  axis.append("a-text")
+  .attr("value", "Z Axis")
+  .attr("position", "0 -0.1 " + chartDepth / 2)
+  .attr("rotation", "0 0 0")
+  .attr("align", "center")
+  .attr("width", "2")
+  .attr("color", "black");
