6b2bb57fd189a0e97612b3370f54eb6a2c4f61ea
Ebube Glory Ogbonda <ogbondaglory@gmail.com>
2023-03-13 17:28:16 +0000

    Update appD3v3.js

--- ICE-4711/js/appD3v3.js
+++ ICE-4711/js/appD3v3.js
@@ -1,45 +1,43 @@
-var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25, 22, 18, 15, 18, 10 ];//
-
-        
+var dataset = [5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25, 22, 18, 15, 18, 10];
 console.log(dataset.length);
 
+var gridMax = Math.sqrt(dataset.length);
+var chartWidth = gridMax * 0.9 + (gridMax - 1) * 0.1;
+var chartDepth = Math.ceil(dataset.length / gridMax) * 0.9 + (Math.ceil(dataset.length / gridMax) - 1) * 0.1;
 
- var gridMax = Math.sqrt(dataset.length);
+var content = d3.select("#helloworld");
 
- var content = d3.select("#helloworld");
-
-// we use d3's enter/update/exit pattern to draw and bind our dom elements
- var myBars = content.selectAll("a-box.bar")
-               .data(dataset)
-               .enter()
-               .append("a-box")
-               .classed("bar", true);
-// we set attributes on our cubes to determine how they are rendered
-
- //var x = -dataset.length/2;
-//var x = 1;
- var y = 1;
- var z = 1;
- var m = 0;
+var myBars = content
+  .selectAll("a-box.bar")
+  .data(dataset)
+  .enter()
+  .append("a-box")
+  .classed("bar", true);
 
 myBars.attr({
-  position: function(d,i) {
-       x=i % gridMax;
-       z=Math.floor(i/gridMax);
-       y=d/4;
-       m ++;
-       console.log("Count: " + m + " - " + "x: " + x + " y: " + y + " z: " + z);
-       return x + " " + y + " " + z;
-       },
-   height: function(d){return d/2;},
-   width: function(d){return 0.9;},
-   depth: function(d){return 0.9;},
-   //radius: function(d){return 0.9/2;},
-   color: function(d){
-     var letters = '0123456789ABCDEF'.split('');
-     var color = '#';
-     for (var i = 0; i < 6; i++) {
-         color += letters[Math.floor(Math.random() * 16)];
-     }
-     return color;}
- });
+  position: function(d, i) {
+    var x = i % gridMax;
+    var z = Math.floor(i / gridMax);
+    var y = d / 4;
+    var posX = x * 1.1 - chartWidth / 2;
+    var posZ = z * 1.1 - chartDepth / 2;
+    return posX + " " + y + " " + posZ;
+  },
+  height: function(d) {
+    return d / 2;
+  },
+  width: function(d) {
+    return 0.9;
+  },
+  depth: function(d) {
+    return 0.9;
+  },
+  color: function(d) {
+    var letters = "0123456789ABCDEF".split("");
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+});
